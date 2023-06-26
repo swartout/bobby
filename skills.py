9d9f9f58b0c87132a84cc7b442bf601e48aa31b8
Carter Swartout <carterswartout@gmail.com>
2023-06-25 19:34:50 -0700

    Make example skills in skills.py

---
+++ skills.py
@@ -0,0 +1,48 @@
+# skills for bobby to use
+# current skills are:
+#  - TurnLightsOn
+#  - TurnLightsOff
+#  - PlayMusic
+#  - StopMusic
+
+# TODO: change all prints to logging
+
+class TurnLightsOn:
+    def name():
+        return "TurnLightsOn"
+
+    def description():
+        return "Turns the lights on to full brightness in the room."
+    
+    def doSkill():
+        print("Turning lights on")
+
+class TurnLightsOff:
+    def name():
+        return "TurnLightsOff"
+
+    def description():
+        return "Turns the lights off in the room."
+    
+    def doSkill():
+        print("Turning lights off")
+
+class PlayMusic:
+    def name():
+        return "PlayMusic"
+
+    def description():
+        return "Plays music from a playlist."
+    
+    def doSkill():
+        print("Playing music")
+
+class StopMusic:
+    def name():
+        return "StopMusic"
+
+    def description():
+        return "Stops playing music."
+    
+    def doSkill():
+        print("Stopping music")
