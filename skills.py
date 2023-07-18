839ad1589859328ee0a4c774c8dce2f52b477d5b
Carter Swartout <carterswartout@gmail.com>
2023-07-18 16:29:51 -0700

    Add self to each "skill"

--- skills.py
+++ skills.py
@@ -1,41 +1,41 @@
 from utils import Skill
 
 class TurnLightsOn(Skill):
-    def name():
+    def name(self):
         return "TurnLightsOn"
 
-    def description():
+    def description(self):
         return "Turns the lights on to full brightness in the room."
     
-    def do_skill():
+    def do_skill(self):
         return "Turning lights on"
 
 class TurnLightsOff(Skill):
-    def name():
+    def name(self):
         return "TurnLightsOff"
 
-    def description():
+    def description(self):
         return "Turns the lights off in the room."
     
-    def do_skill():
+    def do_skill(self):
         return "Turning lights off"
 
 class PlayMusic(Skill):
-    def name():
+    def name(self):
         return "PlayMusic"
 
-    def description():
+    def description(self):
         return "Plays music from a playlist."
     
-    def do_skill():
+    def do_skill(self):
         return "Playing music"
 
 class StopMusic(Skill):
-    def name():
+    def name(self):
         return "StopMusic"
 
-    def description():
+    def description(self):
         return "Stops playing music."
     
-    def do_skill():
-        return "Stopping music"
+    def do_skill(self):
+        return "Error stopping music - music was not playing"
