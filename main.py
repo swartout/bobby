0bf6521fd6fbca9f51cb970b359fa5e416df3605
Carter Swartout <carterswartout@gmail.com>
2023-08-12 20:18:13 -0700

    Add ding sound effect

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ API_KEY = None
 # list of skill names (string) to allow the virtual assistant to use, these
 # skills must be placed in the skills.py file and subclass utils.Skill
 SKILLS = [
-    'EndConversation',
+    'EndConversation', # required by default to end the conversation
     'TurnLightsOff',
     'TurnLightsOn',
     'PlayMusic',
@@ -35,34 +35,36 @@ openai.api_key = API_KEY
 skill_helper = SkillHelper(SKILLS)
 bobby = Bobby(SYSTEM_MESSAGE, skill_helper.get_skills(), MODEL)
 speak = Speak()
-messages = [SYSTEM_MESSAGE]
-
-# get user input
-print("User: ", end="")
-message = {"role": "user", "content": input()}
 
 while True:
-    completion = bobby.get_response(message)
+    speak.ding()
+    messages = [SYSTEM_MESSAGE]
 
-    # do different things if response is chat vs function
-    if completion['finish_reason'] == 'function_call':
-        called_function_name = completion['message']["function_call"]["name"]
-        called_function_args = completion['message']["function_call"]["arguments"]
-        message = completion['message']
-        params = json.loads(called_function_args)
-        if called_function_name == "EndConversation":
-            print("Bobby: " + params['message'])
-            speak.speak(params['message'])
-            break
-        print("Function call: " + called_function_name + ", Args: " + called_function_args)
-        skill_info = skill_helper.do_skill(called_function_name, params=params)
-        message = {"role": "function", "name": called_function_name, "content": skill_info}
-        print("Function info: " + str(skill_info))
-    elif completion['finish_reason'] == 'stop':
-        print("Bobby: " + completion['message']['content'])
-        speak.speak(completion['message']['content'])
-        message = completion['message']
-        print("User: ", end="")
-        message = {"role": "user", "content": input()}
-    else:
-        raise Exception("Invalid response type: not a function call or stop")
+    # get user input
+    print("User: ", end="")
+    message = {"role": "user", "content": input()}
+    while True:
+        completion = bobby.get_response(message)
+
+        # do different things if response is chat vs function
+        if completion['finish_reason'] == 'function_call':
+            called_function_name = completion['message']["function_call"]["name"]
+            called_function_args = completion['message']["function_call"]["arguments"]
+            message = completion['message']
+            params = json.loads(called_function_args)
+            if called_function_name == "EndConversation":
+                print("Bobby: " + params['message'])
+                speak.speak(params['message'])
+                break
+            print("Function call: " + called_function_name + ", Args: " + called_function_args)
+            skill_info = skill_helper.do_skill(called_function_name, params=params)
+            message = {"role": "function", "name": called_function_name, "content": skill_info}
+            print("Function info: " + str(skill_info))
+        elif completion['finish_reason'] == 'stop':
+            print("Bobby: " + completion['message']['content'])
+            speak.speak(completion['message']['content'])
+            message = completion['message']
+            print("User: ", end="")
+            message = {"role": "user", "content": input()}
+        else:
+            raise Exception("Invalid response type: not a function call or stop")
