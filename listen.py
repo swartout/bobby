e353f82c840e33b3b657468f86bf64dd3bee62c3
ramblecube <16677360+ramblecube@users.noreply.github.com>
2023-08-12 22:09:33 -0700

    Finished up the listener class

--- listen.py
+++ listen.py
@@ -3,15 +3,17 @@ import speech_recognition as sr
 
 class Listen:
     
+    """Calibrate the microphone"""
     def __init__(self, api_key = None):
         self.api_key = api_key
+        with sr.Microphone(device_index=1) as source:
+            print("Calibrating!")
+            r.adjust_for_ambient_noise(source, duration=2)
     
+    """Transcribe some text"""
     def listen(self):
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("[DBG] Calibrating...")
-            r.adjust_for_ambient_noise(source, duration=5)  
-            r.dynamic_energy_threshold = True  
-            print("[DBG] Listening...")
+        with sr.Microphone(device_index=1) as source:
+            print("Listening!")
             audio = r.listen(source)
-            return r.recognize_whisper_api(audio, api_key=api_key)
+            return r.recognize_whisper_api(audio, api_key=self.api_key)
