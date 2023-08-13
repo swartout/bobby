d7af097567de40aa739c33d8a382a109c641ae12
Carter Swartout <carterswartout@gmail.com>
2023-08-12 22:51:40 -0700

    Add speech recognition ability

--- listen.py
+++ listen.py
@@ -6,14 +6,15 @@ class Listen:
     """Calibrate the microphone"""
     def __init__(self, api_key = None):
         self.api_key = api_key
-        with sr.Microphone(device_index=1) as source:
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
             print("Calibrating!")
             r.adjust_for_ambient_noise(source, duration=2)
     
     """Transcribe some text"""
     def listen(self):
         r = sr.Recognizer()
-        with sr.Microphone(device_index=1) as source:
+        with sr.Microphone() as source:
             print("Listening!")
             audio = r.listen(source)
             return r.recognize_whisper_api(audio, api_key=self.api_key)
