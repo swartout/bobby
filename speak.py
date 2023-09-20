64129c87eb72c0a8fe5ff2ba9251be9dd8b5bb4e
ramblecube <16677360+ramblecube@users.noreply.github.com>
2023-09-20 16:13:13 -0700

    Implemented new sounds

--- speak.py
+++ speak.py
@@ -1,6 +1,9 @@
 """Synthesizes speech from a string using Elevenlabs Text-to-Speech API."""
 from elevenlabs import generate, stream
 from playsound import playsound
+import wave
+import threading
+import pyaudio
 
 class Speak:
     """Speak class"""
@@ -13,6 +16,32 @@ class Speak:
         """
         self.voice = voice
         self.api_key = api_key
+        self.thinking = False
+        self.think_thread = None
+        
+    def _play_think(self):
+        """Private method to play think.mp3 in a loop using pyAudio."""
+        chunk = 1024
+        wf = wave.open('sound/think.wav', 'rb')
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+
+        data = wf.readframes(chunk)
+
+        while self.thinking and data:
+            stream.write(data)
+            data = wf.readframes(chunk)
+            if not data:  # If file end is reached, loop from the beginning.
+                wf.rewind()
+                data = wf.readframes(chunk)
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
 
     def speak(self, text):
         """Speak text
@@ -28,6 +57,31 @@ class Speak:
         )
         stream(audio)
 
-    def ding(self):
-        """Play a ding sound"""
-        playsound('ding.mp3')
+    def hearing(self):
+        """Play a hearing sound"""
+        playsound('sound/hearing.mp3')
+        
+    def heard(self):
+        """Play a heard sound"""
+        playsound('sound/heard.mp3')
+        
+    def think(self):
+        """Play a think sound in a loop using a separate thread."""
+        self.thinking = True
+        self.think_thread = threading.Thread(target=self._play_think)
+        self.think_thread.start()
+        
+    def stop_think(self):
+        """Stop the think sound."""
+        self.thinking = False
+        if self.think_thread:
+            self.think_thread.join()  # Wait for the thread to finish.
+        playsound('sound/speak.mp3')
+    
+    def speaksnd(self):
+        """Play a sound right before speaking"""
+        playsound('sound/think.mp3')
+    
+    def done(self):
+        """Play a done speaking sound"""
+        playsound('sound/done.mp3')
